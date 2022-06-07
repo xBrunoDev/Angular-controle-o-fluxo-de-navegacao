4117c8d9d14b6838cbd5332c47e5eb2d85b81ef4
xBrunoDev <bruno_gabriel06@hotmail.com>
2022-06-07 11:32:37 -0300

    Configurando Environment

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -1,10 +1,11 @@
+import { environment } from './../../environments/environment.prod';
 import { TokenService } from './../autenticacao/token.service';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Animais } from './animais';
 
-const API='htttp://localhost:3000';
+const API = environment.apiURL;
 
 @Injectable({
   providedIn: 'root'

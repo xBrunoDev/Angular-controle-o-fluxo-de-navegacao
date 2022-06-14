cc691ab72c7714c5e51624b51233d0d991477e45
xBrunoDev <bruno_gabriel06@hotmail.com>
2022-06-14 12:06:35 -0300

    Serviço ActivatedRoute

---
+++ src/app/animais/detalhe-animal/detalhe-animal.component.ts
@@ -0,0 +1,24 @@
+import { AnimaisService } from './../animais.service';
+import { Animal } from './../animais';
+import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-detalhe-animal',
+  templateUrl: './detalhe-animal.component.html',
+  styleUrls: ['./detalhe-animal.component.css']
+})
+export class DetalheAnimalComponent implements OnInit {
+
+  animalID!:number;
+  animal$!:Observable<Animal>;
+
+  constructor(private animaisService:AnimaisService, private activatedRoute:ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.animalID = this.activatedRoute.snapshot.params.animalId;
+    this.animal$ = this.animaisService.buscaPorID(this.animalID);
+  }
+
+}

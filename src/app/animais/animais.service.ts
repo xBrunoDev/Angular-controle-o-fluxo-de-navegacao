ca3de59fea5eaf9f7318346bf225ceb400b404a3
xBrunoDev <bruno_gabriel06@hotmail.com>
2022-06-14 15:46:24 -0300

    Curtir e Excluir foto

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -1,11 +1,13 @@
 import { environment } from './../../environments/environment.prod';
 import { TokenService } from './../autenticacao/token.service';
-import { Observable } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Animais, Animal } from './animais';
+import { catchError, mapTo } from 'rxjs/operators';
 
 const API = environment.apiURL;
+const NOT_MODIFIED = '304' ;
 
 @Injectable({
   providedIn: 'root'
@@ -22,4 +24,18 @@ export class AnimaisService {
     return this.http.get<Animal>(` ${API}/photos/${id}`);
   }
 
+  excluiAnimal(id:number):Observable<Animal>{
+  return this.http.delete<Animal>(`${API}/photos/${id}`);
+  }
+
+  curtir(id:number): Observable<boolean>{
+    return this.http.post(
+      `${API}/photos/${id}/like`,
+       {},
+       {observe: 'response'}
+       ).pipe(
+        mapTo(true),catchError((error) => {
+          return error.status === NOT_MODIFIED ? of(false) : throwError(error);
+        }));
+  }
 }
--- src/app/animais/detalhe-animal/detalhe-animal.component.ts
+++ src/app/animais/detalhe-animal/detalhe-animal.component.ts
@@ -1,8 +1,9 @@
+import { error } from 'console';
 import { AnimaisService } from './../animais.service';
 import { Animal } from './../animais';
 import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-detalhe-animal',
@@ -14,11 +15,31 @@ export class DetalheAnimalComponent implements OnInit {
   animalID!:number;
   animal$!:Observable<Animal>;
 
-  constructor(private animaisService:AnimaisService, private activatedRoute:ActivatedRoute) { }
+  constructor(
+    private animaisService:AnimaisService,
+    private activatedRoute:ActivatedRoute,
+    private router: Router,
+    ) { }
 
   ngOnInit(): void {
     this.animalID = this.activatedRoute.snapshot.params.animalId;
     this.animal$ = this.animaisService.buscaPorID(this.animalID);
+  }
+
+  curtir() {
+    this.animaisService.curtir(this.animalID).subscribe((curtida) => {
+      if (curtida) {
+        this.animal$ = this.animaisService.buscaPorID(this.animalID);
+      }
+    });
+  }
+
+  excluir() {
+    this.animaisService.excluiAnimal(this.animalID).subscribe(() => {
+      this.router.navigate(['/animais/']);
+    },
+    (error) => console.log(error)
+    );
   }
 
 }
